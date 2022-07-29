4822b881e4f0d15a304aca4d95bb81b1ce00ad73
Alyaa <alyaa.moh.bakr@gmail.com>
2022-07-29 15:32:53 +0200

    Initial Commit

---
+++ RST_Sync/RST_Sync.v
@@ -0,0 +1,33 @@
+module RST_Sync #(parameter NUM_STAGES = 2)
+(
+    input   wire                    RST                                 ,
+    input   wire                    CLK                                 ,
+
+    output  reg                     Sync_RST    
+);
+
+/********************************************************************************/
+/********************************************************************************/
+
+    reg          [NUM_STAGES-1:0]   register                            ;
+
+/********************************************************************************/
+/********************************************************************************/
+
+    always @(posedge CLK or negedge RST) 
+        begin
+
+            if(!RST)
+                begin
+
+                    register <= 'b0                                     ;
+                    Sync_RST <= 'b0                                     ; 
+                
+                end
+            else
+            
+                {Sync_RST,register} <= {register[NUM_STAGES-1:0],1'b1}  ;
+
+        end
+
+endmodule
