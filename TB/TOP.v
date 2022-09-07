77ce1e4aefd2580065758b1a76121bcc651cd902
Alyaa <alyaa.moh.bakr@gmail.com>
2022-09-07 18:11:49 +0200

    Update

--- TB/TOP.v
+++ TB/TOP.v
@@ -247,7 +247,7 @@ module TOP #(parameter ADDRESS_WIDTH = 4, REG_DEPTH = 16, ALU_FUN_WIDTH = 4, ALU
 /*****************************************************************************************/
 /***************Raise the Data & Valid for a certain time to meet the edge****************/
 
-    always@(posedge REF_CLK or RST_REF)
+    always@(posedge REF_CLK or negedge RST_REF)
         begin
 
             if(!RST_UART)
